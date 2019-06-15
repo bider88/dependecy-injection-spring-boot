e122c83e325138e3d0e0268abe0ea4e1214a2efd
Irving Didier <bider88@gmail.com>
2019-06-15 12:28:42 -0500

    Example Invoice and relation with their components

---
+++ src/main/java/com/dependecyinjection/springboot/app/models/domain/Invoice.java
@@ -0,0 +1,37 @@
+package com.dependecyinjection.springboot.app.models.domain;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Invoice {
+
+	@Value("${description.invoice}")
+	private String description;
+	@Autowired
+	private Client client;
+	private List<ItemInvoice> items;
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	public List<ItemInvoice> getItems() {
+		return items;
+	}
+	public void setItems(List<ItemInvoice> items) {
+		this.items = items;
+	}
+	
+}
